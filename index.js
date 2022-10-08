f570c950d769682041603140480b20f01334399a
Giuliano Machado <giufma@gmail.com>
2022-10-08 13:23:36 -0300

    remove bugs from socket.io code

--- index.js
+++ index.js
@@ -29,20 +29,20 @@ io.sockets.on('connection', function(socket) {
     log('Received request to create or join room '+ room);
 
     const clientsInRoom = io.sockets.adapter.rooms[room];
-    const numClients = clientsInRoom ? Object.keys(clientsInRoom).length : 0;
+    const numClients = clientsInRoom ? clientsInRoom.length : 0;
 
-    log(`Room ${room} now has ${numClients} client(s)`);
+    log(`Room ${room} now has ${numClients + 1} client(s)`);
 
     if (numClients === 0) {
-      socket.join(room);
       log(`Client ID ${socket.id} created room ${room}`);
+      socket.join(room);
       socket.emit('created', room, socket.id);
     } else if (numClients === 1) {
       log(`Client ID ${socket.id} joined room ${room}`);
       io.sockets.in(room).emit('join', room);
       socket.join(room);
       socket.emit('joined', room, socket.id);
-      io.socket.in(room).emit('ready');
+      io.sockets.in(room).emit('ready');
     } else { // max two clients
       socket.emit('full', room);
     }
