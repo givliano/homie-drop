95c3b476cbbbc50f9052bad6bef67ce4a45dde64
Giuliano <giufma@gmail.com>
2022-12-07 17:02:32 -0300

    remove unused elements

--- src/pages/index.js
+++ src/pages/index.js
@@ -134,9 +134,7 @@ function HomePage() {
       </div>
 
       <div id="buttons">
-        <button id="snap">Snap</button><span> then </span><button id="send">Send</button>
-        <span> or </span>
-        <button id="snapAndSend">Snap &amp; Send</button>
+        <button id="send" onClick={() => peer.sendPhoto()}>Send</button>
       </div>
 
       <div id="incoming">
