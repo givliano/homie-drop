5f3c0c12ee786c280caf48b896a8062f1ca78eeb
Giuliano <giufma@gmail.com>
2022-12-10 01:36:56 -0300

    implement more complex FilePicker

--- src/pages/FilePicker.js
+++ src/pages/FilePicker.js
@@ -1,10 +1,17 @@
 export function FilePicker({ onChange }) {
   return (
-    <input
-      type="file"
-      id="input"
-      multiple
-      onChange={onChange}
-    />
+    <div className="file-picker__wrapper">
+      <div className="file-picker__info-container">
+        <p className="file-picker__info-text">Drag your photos or videos here</p>
+        <p className="file-picker__info-text">Or click on this area</p>
+      </div>
+      <input
+        className="file-picker__input"
+        type="file"
+        id="input"
+        multiple
+        onChange={onChange}
+      />
+    </div>
   )
 }
