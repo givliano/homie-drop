5569ad95658c30e100b378e28cf3aa1662f873dc
Giuliano <giufma@gmail.com>
2022-12-20 23:54:43 -0300

    replace qrcode with kjua

--- src/components/QrModal.js
+++ src/components/QrModal.js
@@ -1,10 +1,17 @@
 import { useEffect, useState } from 'react';
+import { Kjua } from 'react-kjua';
 
-export const QrModal = ({ img, active }) => {
+export const QrModal = ({ img, active, text }) => {
+  const [url, setUrl] = useState(null);
+
+  useEffect(() => {
+    setUrl(window.location.href);
+  }, []);
+
   return (
     <div className={`modal-wrapper ${active}`}>
       <button className="btn-close">X</button>
-      <img src={img} />
+      { url && <Kjua text={url} /> }
     </div>
   );
 }
