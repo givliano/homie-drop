6dc20b5173dfcf12f0e10a3732d39a3a44c0e3c1
Giuliano <giufma@gmail.com>
2022-12-25 14:53:08 -0300

    start refactoring file preview to be text only

--- src/components/FilePreview.js
+++ src/components/FilePreview.js
@@ -1,9 +1,17 @@
-export const FilePreview = ({ active = '', multipleFiles }) => {
+
+export const FilePreview = ({ active = '', multipleFiles, transferring }) => {
   const adjustWrapper = multipleFiles ? 'adjust-wrapper' : '';
 
   return (
     <div className={`file-preview__wrapper ${active} ${adjustWrapper}`}>
-      <div id='preview' className={adjustWrapper}></div>
+      <div id='preview' className={adjustWrapper}>{transferring &&
+        <>
+          <div>Percentage: 0</div>
+          <div>{transferring.name}</div>
+          <div>{transferring.size}</div>
+          <div>{transferring.type}</div>
+        </>
+      }</div>
     </div>
   );
 }
