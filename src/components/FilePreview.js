15c1dedcf15879f6ec0aed1e588e7bc56a35f10a
Giuliano <giufma@gmail.com>
2023-01-17 18:24:18 -0300

    add logic for the transfer progress in rect and file preview

--- src/components/FilePreview.js
+++ src/components/FilePreview.js
@@ -1,6 +1,29 @@
+import { useEffect, useState } from "react";
+import { useTransferProgress } from '../hooks/useTransferProgress';
+import { useInitTransfer } from '../hooks/useInitTransfer';
 
 export const FilePreview = ({ active = '', multipleFiles, transferring }) => {
   const adjustWrapper = multipleFiles ? 'adjust-wrapper' : '';
+
+  const fileInfo = useInitTransfer();
+  // Gets the byte size of the received message from the channel.
+  const transferProgress = useTransferProgress();
+  // Holds the percentage of the amount transferred compared to the total size.
+  const [percentage, setPercentage] = useState(0);
+
+  const handleProgress = () => {
+    // Float multiplication is very annoying sometimes. Fixing the decimal
+    // for last is necessary so it doesn't generate many decimals points.
+    setPercentage((transferProgress / fileInfo.size * 100).toFixed(2));
+  }
+
+  useEffect(() => {
+    if (!fileInfo) {
+      return;
+    }
+
+    handleProgress();
+  }, [transferProgress])
 
   return (
     <div className={`file-preview__wrapper ${active} ${adjustWrapper}`}>
--- src/components/Rect.js
+++ src/components/Rect.js
@@ -1,10 +1,54 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { animate } from '../lib/utils';
+import { useTransferProgress } from '../hooks/useTransferProgress';
+import { useInitTransfer } from '../hooks/useInitTransfer';
 
 const Rect = () => {
   const blueRect = useRef(null);
   const maskRect = useRef(null);
   const pinkRect = useRef(null);
+
+  const fileInfo = useInitTransfer();
+  const transferProgress = useTransferProgress();
+  const [percentage, setPercentage] = useState(0);
+  const [isVisible, setIsVisible] = useState(false);
+
+  function handleProgress() {
+    setPercentage((transferProgress / fileInfo.size * 100).toFixed(2));
+  }
+
+  useEffect(() => {
+    if (!fileInfo) {
+      return;
+    }
+    handleProgress();
+  }, [transferProgress]);
+
+  useEffect(() => {
+    if (!fileInfo) {
+      return;
+    }
+
+    if (!isVisible) {
+      blueRect.current.style.opacity = 1;
+      setIsVisible(true);
+    }
+
+    blueRect.current.style.strokeDashoffset = (1000 - (10 * percentage));
+    // animatePath(blueRect.current, (percentage * 10));
+
+    if (percentage === 1) {
+      blueRect.current.style.strokeDashoffset = 1000;
+      blueRect.current.style.opacity = 0;
+      setIsVisible(false);
+      setPercentage(0);
+    }
+  }, [percentage])
+
+  useEffect(() => {
+    console.log('FILE INFO', fileInfo);
+  }, [fileInfo]);
+
 
   function animatePath(el, distance) {
     if (el !== null) {
@@ -19,13 +63,6 @@ const Rect = () => {
       });
     }
   }
-
-  useEffect(() => {
-    setTimeout(() => {
-      blueRect.current.style.opacity = 1;
-      animatePath(blueRect.current, 500);
-    }, 2000);
-  }, [])
 
   return (
     <svg xmlns="http://www.w3.org/2000/svg" className="rect" viewBox="0 0 300 200">
